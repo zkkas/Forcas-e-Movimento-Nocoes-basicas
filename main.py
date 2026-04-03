f7645ca5abaa8b7b4b68b123f4ff1bcf948fb343
zkkas <gabrielpcgamer23@gmail.com>
2026-04-03 14:47:51 -0300

    adicionando as variaveis x e y

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # dados de x e y para o gráfico
-x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-y = np.array([2.1, 3.9, 6.2, 7.8, 10.1, 12.3, 13.9, 16.2, 18.0, 19.8, 22.1, 24.3, 25.9, 28.1, 30.2])
+x = np.array([])
+y = np.array([])
 
 # criando o gráfico de dispersão
