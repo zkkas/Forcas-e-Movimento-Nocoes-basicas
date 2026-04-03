04fa003add2b78f23024900a1030f354e158d3c8
zkkas <gabrielpcgamer23@gmail.com>
2026-04-03 15:01:16 -0300

    add dos graficos

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+#importando as bibliotecas necessárias
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 # dados de x e y para o gráfico
-x = np.array([])
-y = np.array([])
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 4, 6, 8, 10])
 
-# criando o gráfico de dispersão
+# gráfico de dispersão
+plt.scatter(x, y) #e possivel melhorar a logica do grafico
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+plt.grid()
+plt.show()
