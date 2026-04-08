106158f11ac639fc82a73c9eb2684b38d382104b
zkkas <gabrielpcgamer23@gmail.com>
2026-04-07 22:36:50 -0300

    melhoria do codigo

--- main.py
+++ main.py
@@ -3,12 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 # dados de x e y para o gráfico
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([2, 4, 6, 8, 10])
+x = [1,2,3,4,5]
+y = [2,5,7,8,11]
+# função do grafico
+def plot_graph(x, y,):
+    #variáveis para o título e os rótulos dos eixos
+    title = "Gráfico de Dispersão"
+    xlabel = "Eixo X"
+    ylabel = "Eixo Y"
+    # função da reta
+    z = np.polyfit(x,y,1)
 
-# gráfico de dispersão
-plt.scatter(x, y) #e possivel melhorar a logica do grafico
-plt.xlabel('Eixo X')
-plt.ylabel('Eixo Y')
-plt.grid()
-plt.show()
+    a = z[0]
+    b = z[1]
+
+    #grafico de dispersão
+    plt.scatter(x, y, label ="Dados Experimentais")
+
+    #função da reta
+    p = np.poly1d(z)
+
+    #equação 
+    equacao = (f"y = {a:.2f} x +{b:.2f}")
+
+    #linha de tendência
+    plt.plot(x, p(x), "r--", label = "Linha de Tendência")
+
+    #configurações do gráfico
+    plt.title(title)
+    plt.text(1,9,equacao, fontsize=12, bbox=dict(facecolor='white'))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+# chamada da função do grafico
+plot_graph(x, y)
